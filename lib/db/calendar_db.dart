61c1aa7edf038e34d2fc4258b42b19b088568090
Majeed <md9mgoody@gmail.com>
2024-09-26 11:34:36 +0300

    for i can pull from github repo

--- lib/db/calendar_db.dart
+++ lib/db/calendar_db.dart
@@ -1,126 +1,9 @@
-Map<String, List<Map<String, dynamic>>> calendarDB = {
+Map<String, List<Map<String, dynamic>>> calendar = {
   "seasons": [
     {"SeasonID": 1, "SeasonName": "الشتاء"},
     {"SeasonID": 2, "SeasonName": "الربيع"},
     {"SeasonID": 3, "SeasonName": "الصيف"},
     {"SeasonID": 4, "SeasonName": "الخريف"}
-  ],
-  "January": [
-    {
-      "id": 1,
-      "name": "January",
-      "number": 1,
-      "season": "Winter",
-      "stars": ["الهنعة", "الذراع", "النثرة"],
-    },
-  ],
-  "February": [
-    {
-      "id": 2,
-      "name": "February",
-      "number": 2,
-      "season": "Winter",
-      "stars": ["الطرف", "الجبهة"],
-    },
-  ],
-  "March": [
-    {
-      "id": 3,
-      "name": "March",
-      "number": 3,
-      "season": "Spring",
-      "stars": ["الزبرة", "الصرفه"],
-    },
-  ],
-  "April": [
-    {
-      "id": 4,
-      "name": "April",
-      "number": 4,
-      "season": "Spring",
-      "stars": ["العواء", "السماك"],
-    },
-  ],
-  "May": [
-    {
-      "id": 5,
-      "name": "May",
-      "number": 5,
-      "season": "Spring",
-      "stars": ["الغفر", "الزبان"],
-    },
-  ],
-  "June": [
-    {
-      "id": 6,
-      "name": "June",
-      "number": 6,
-      "season": "Summer",
-      "stars": ["إكليل", "القلب"],
-    },
-  ],
-  "July": [
-    {
-      "id": 7,
-      "name": "July",
-      "number": 7,
-      "season": "Summer",
-      "stars": ["الشول", "النعائم"],
-    },
-  ],
-  "August": [
-    {
-      "id": 8,
-      "name": "August",
-      "number": 8,
-      "season": "Summer",
-      "stars": ["البلدة", "المرزم"],
-    },
-  ],
-  "September": [
-    {
-      "id": 9,
-      "name": "September",
-      "number": 9,
-      "season": "Fall",
-      "stars": ["سهيل", "باعريق"],
-    },
-  ],
-  "October": [
-    {
-      "id": 10,
-      "name": "October",
-      "number": 10,
-      "season": "Fall",
-      "stars": ["سعد (خباء)", "الفرغ"],
-    },
-  ],
-  "November": [
-    {
-      "id": 11,
-      "name": "November",
-      "number": 11,
-      "season": "Fall",
-      "stars": ["الدلو", "الحوت"],
-    },
-  ],
-  "December": [
-    {
-      "id": 12,
-      "name": "December",
-      "number": 12,
-      "season": "Winter",
-      "stars": ["النطح", "البطين"],
-    },
-  ],
-  "Extra": [
-    {
-      "id": 13,
-      "name": "Extra",
-      "number": 13,
-      "season": "Various",
-      "stars": ["الثريا", "البركان", "الهقعة"],
-    },
   ],
   "stars": [
     {
@@ -178,7 +61,7 @@ Map<String, List<Map<String, dynamic>>> calendarDB = {
       "EndDate": "2024-03-31",
       "SeasonID": 2,
       "Duration": 13
-    }
+    },
   ],
   "crops": [
     {
@@ -229,5 +112,14 @@ Map<String, List<Map<String, dynamic>>> calendarDB = {
       "Description": "تجميع العسل من الطرف، الجبهة، الزبرة، الصرفة",
       "RelatedStars": "الطرف، الجبهة، الزبرة، الصرفة"
     }
+  ],
+  "starSeasons": [
+    {"StarSeasonID": 1, "StarID": 1, "SeasonID": 1},
+    {"StarSeasonID": 2, "StarID": 2, "SeasonID": 1},
+    {"StarSeasonID": 3, "StarID": 3, "SeasonID": 1},
+    {"StarSeasonID": 4, "StarID": 4, "SeasonID": 1},
+    {"StarSeasonID": 5, "StarID": 5, "SeasonID": 1},
+    {"StarSeasonID": 6, "StarID": 6, "SeasonID": 2},
+    {"StarSeasonID": 7, "StarID": 7, "SeasonID": 2}
   ]
 };

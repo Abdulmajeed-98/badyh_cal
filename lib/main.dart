c9a20e68f04ec2145f02a1b5fa43b9a59b4d6f75
Yusuf <yusuf711ahmed@gmail.com>
2024-10-14 03:24:46 +0300

    Empty commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:badiyh_calendar/core/theme/app_theme.dart';
 import 'package:badiyh_calendar/helpers/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
